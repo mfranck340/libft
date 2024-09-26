c5b8b835a89c8c0b190b875b4f9712378958797f
mfranck340 <franck.yaguargos@edu.uah.es>
2024-09-27 00:52:18 +0200

    add putendl putnbr putstr fix itoa

--- files/ft_itoa.c
+++ files/ft_itoa.c
@@ -12,31 +12,43 @@
 
 #include "stdlib.h"
 
+static int	count_digits(int n)
+{
+	int	count;
+
+	count = 0;
+	if (n <= 0)
+		count++;
+	while (n /= 10)
+		count++;
+	return (count);
+}
+
 char	*ft_itoa(int n)
 {
 	char	*str;
 	int		len;
-	int		neg;
 
-	len = 1;
-	neg = 0;
-	if (n < 0)
-	{
-		neg = 1;
-		len++;
-	}
-	while (n /= 10)
-		len++;
+	len = count_digits(n);
 	str = (char *) malloc(sizeof(char) * (len + 1));
 	if (!str)
 		return (0);
 	str[len] = '\0';
-	while (n != 0)
+	if (n == -2147483648)
 	{
-		str[--len] = n % 10 + '0';
+		str[--len] = '8';
+		n = n / 10;
+	}
+	if (n < 0)
+	{
+		str[0] = '-';
+		n *= -1;
+	}
+	while (n > 9)
+	{
+		str[--len] = (n % 10) + '0';
 		n /= 10;
 	}
-	if (neg)
-		str[0] = '-';
+	str[--len] = n + '0';
 	return (str);
 }
---
+++ files/ft_putnbr_fd.c
@@ -0,0 +1,10 @@
+
+
+void	ft_putnbr_fd(int n, int fd)
+{
+	char	*str;
+
+	str = ft_itoa(n);
+	ft_putstr_fd(str, fd);
+	free(str);
+}
