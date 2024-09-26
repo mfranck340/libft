546e38840e6671c7e13e21f6db9398d940daad55
mfranck340 <franck.yaguargos@edu.uah.es>
2024-09-26 15:33:27 +0200

    add strjoin

---
+++ files/ft_strjoin.c
@@ -0,0 +1,15 @@
+#include "libft.h"
+
+char	*ft_strjoin(char const *s1, char const *s2)
+{
+	char	*join;
+
+	if (!s1 || !s2)
+		return (0);
+	join = (char *)malloc(sizeof(char) * (ft_strlen(s1) + ft_strlen(s2) + 1));
+	if (!join)
+		return (0);
+	ft_strlcpy(join, s1, ft_strlen(s1) + 1);
+	ft_strlcat(join, s2, ft_strlen(s1) + ft_strlen(s2) + 1);
+	return (join);
+}
