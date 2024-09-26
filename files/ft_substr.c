65f8a23cc0535131389386c5c5f442e317ebbf24
mfranck340 <franck.yaguargos@edu.uah.es>
2024-09-26 15:06:35 +0200

    add modified and function substr

---
+++ files/ft_substr.c
@@ -0,0 +1,19 @@
+
+#include "libft.h"
+
+char	*ft_substr(char const *s, unsigned int start, size_t len)
+{
+	char	*sub;
+
+	if (!s)
+		return (0);
+	if (start >= ft_strlen(s))
+		return (ft_strdup(""));
+	if (ft_strlen(s + start) < len)
+		len = ft_strlen(s + start);
+	sub = (char *)malloc(sizeof(char) * (len + 1));
+	if (!sub)
+		return (0);
+	ft_strlcpy(sub, s + start, len + 1);
+	return (sub);
+}
