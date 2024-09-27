7fc9e946d8ebcd07d4b60c233b390313d9118488
mfranck340 <franck.yaguargos@edu.uah.es>
2024-09-27 20:52:56 +0200

    modified lstmap

--- files/ft_lstmap.c
+++ files/ft_lstmap.c
@@ -16,6 +16,7 @@ t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *))
 {
 	t_list	*new_list;
 	t_list	*tmp;
+	t_list	*new_node;
 
 	if (!lst || !f)
 		return (0);
@@ -26,14 +27,16 @@ t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *))
 	lst = lst->next;
 	while (lst)
 	{
-		tmp->next = ft_lstnew(f(lst->content));
-		if (!tmp->next)
+		new_node = ft_lstnew(f(lst->content));
+		if (!new_node)
 		{
 			ft_lstclear(&new_list, del);
 			return (0);
 		}
+		tmp->next = new_node;
 		tmp = tmp->next;
 		lst = lst->next;
 	}
 	return (new_list);
 }
+
